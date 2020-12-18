8782d56f81491400b52f0cdfaa0dc890578d1a88
fsanchir <64943969+fsanchir@users.noreply.github.com>
2020-12-18 15:35:00 +0900

    Update mcopener.py

--- MCopener/source/mcopener.py
+++ MCopener/source/mcopener.py
@@ -19,5 +19,5 @@ while True:
     os.system(f"ngrok authtoken {token} >nul")
     os.system("timeout /t 1 /nobreak >nul")
     os.system("ngrok tcp 25565 --region=jp")
-    print("프로그램이 종료되었습니다. 만약 갑자기 꺼졌다면 아무 키를 눌러 다시 시작하세요.<br><br>다시 시작하시겠습니까? 다시 시작하시려면 아무 키나 누르세요.\n다시 시작하지 않으시려면 X키를 눌러 조의를 표하시지 마시고 종료하세요.")
+    print("프로그램이 종료되었습니다. 만약 갑자기 꺼졌다면 아무 키를 눌러 다시 시작하세요.\n\n다시 시작하시겠습니까? 다시 시작하시려면 아무 키나 누르세요.\n다시 시작하지 않으시려면 X키를 눌러 조의를 표하시지 마시고 종료하세요.")
     os.system("pause")
